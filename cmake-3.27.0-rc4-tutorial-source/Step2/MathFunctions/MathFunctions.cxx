5d22009eae1a8ced3a08ec2660bb264df2da18a3
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-07-01 21:18:09 +0100

    finished Step2: adding an option (ex2)

--- cmake-3.27.0-rc4-tutorial-source/Step2/MathFunctions/MathFunctions.cxx
+++ cmake-3.27.0-rc4-tutorial-source/Step2/MathFunctions/MathFunctions.cxx
@@ -1,15 +1,22 @@
 #include "MathFunctions.h"
 
+#ifdef USE_MYMATH
 #include "mysqrt.h"  // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
-
+#else
 // TODO 11: include cmath
+#include <cmath>
+#endif
 
 namespace mathfunctions {
 
 double sqrt(double x) {
     // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
     // Otherwise, use std::sqrt.
+#ifdef USE_MYMATH
     return detail::mysqrt(x);
+#else
+    return std::sqrt(x);
+#endif  // DEBUG
 }
 
 }  // namespace mathfunctions
