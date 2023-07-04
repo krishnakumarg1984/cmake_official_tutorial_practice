dfc32eea1748b5d2470350a657dd40e02c3e94d7
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-07-04 21:46:40 +0100

    Step4: prepared a starting point

--- cmake-3.27.0-rc4-tutorial-source/Step4/tutorial.cxx
+++ cmake-3.27.0-rc4-tutorial-source/Step4/tutorial.cxx
@@ -1,27 +1,26 @@
 // A simple program that computes the square root of a number
+#include "MathFunctions.h"
+#include "TutorialConfig.h"
+
 #include <cmath>
 #include <iostream>
 #include <string>
 
-#include "MathFunctions.h"
-#include "TutorialConfig.h"
-
-int main(int argc, char* argv[])
-{
+int main(int argc, const char* argv[]) {
   if (argc < 2) {
     // report version
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        std::cout << argv[0] << " Version: " << Tutorial_VERSION_MAJOR << '.' << Tutorial_VERSION_MINOR << '\n';
+        std::cout << "Usage: " << argv[0] << " number\n";
+
     return 1;
   }
 
   // convert input to double
-  const double inputValue = std::stod(argv[1]);
+    const double inputValue { std::stod(argv[1]) };
 
   const double outputValue = mathfunctions::sqrt(inputValue);
 
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
+    std::cout << "The square root of " << inputValue << " is " << outputValue << '\n';
+
   return 0;
 }
