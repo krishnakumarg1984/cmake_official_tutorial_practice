edae8fd1c33a256dc17690fed8d1391bfd84c111
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-07-04 22:03:31 +0100

    finished Step4: adding generator expressions

--- cmake-3.27.0-rc4-tutorial-source/Step4/tutorial.cxx
+++ cmake-3.27.0-rc4-tutorial-source/Step4/tutorial.cxx
@@ -3,24 +3,25 @@
 #include "TutorialConfig.h"
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 
 int main(int argc, const char* argv[]) {
-  if (argc < 2) {
-    // report version
+    if ( argc < 2 ) {
+        // report version
         std::cout << argv[0] << " Version: " << Tutorial_VERSION_MAJOR << '.' << Tutorial_VERSION_MINOR << '\n';
         std::cout << "Usage: " << argv[0] << " number\n";
 
-    return 1;
-  }
+        return EXIT_FAILURE;
+    }
 
-  // convert input to double
+    // convert input to double
     const double inputValue { std::stod(argv[1]) };
 
-  const double outputValue = mathfunctions::sqrt(inputValue);
+    const double outputValue = mathfunctions::sqrt(inputValue);
 
     std::cout << "The square root of " << inputValue << " is " << outputValue << '\n';
 
-  return 0;
+    return 0;
 }
